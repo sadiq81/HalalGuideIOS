1648912ac6a57e9b9d4b693923afddeaa8cd3859
Tommy Sadiq Hinrichsen <tommy.lynge@gmail.com>
2014-11-09 21:48:08 +0100

    Initial Commit

---
+++ HalalGuide/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HalalGuide
+//
+//  Created by Privat on 09/11/14.
+//  Copyright (c) 2014 Eazy It. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HalalGuide/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HalalGuide
+//
+//  Created by Privat on 09/11/14.
+//  Copyright (c) 2014 Eazy It. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
