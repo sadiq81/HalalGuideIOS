59b052349fd6a57e81067e2d29f8eb7631cedc7e
Privat <tommy.lynge@gmail.com>
2015-04-25 21:03:52 +0200

    new Location image picker like review image picker #96

---
+++ HalalGuide/UI/CreateLocation/Views/HGNewLocationPictureCell.h
@@ -0,0 +1,15 @@
+//
+// Created by Privat on 25/04/15.
+// Copyright (c) 2015 Eazy It. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+
+@interface HGNewLocationPictureCell : UICollectionViewCell
+
+@property(strong, nonatomic, readonly) UIImageView *imageView;
+@property(strong, nonatomic, readonly) UIImageView *delete;
+
+@end
