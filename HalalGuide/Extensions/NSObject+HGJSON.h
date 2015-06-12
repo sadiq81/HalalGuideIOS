80e82bb9121eab5e677481ff9277d00053c59ac5
Tommy Sadiq Hinrichsen <tommy.lynge@gmail.com>
2015-06-12 21:53:30 +0200

    Upload image in background using Background Transfer Service #98

---
+++ HalalGuide/Extensions/NSObject+HGJSON.h
@@ -0,0 +1,12 @@
+//
+// Created by Privat on 12/06/15.
+// Copyright (c) 2015 Eazy It. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (HGJSON)
+
+- (NSString *)bv_jsonDataWithPrettyPrint:(BOOL)prettyPrint;
+
+@end
