77c488c590b26b0bd1d54bcfb17ed9ac01e7835a
Tommy Sadiq Hinrichsen <tommy.lynge@gmail.com>
2015-06-20 12:53:49 +0200

    Some authenticated users are not subscribed to global channel #118

--- HalalGuide/Supporting Files/code/HGAppDelegate.m
+++ HalalGuide/Supporting Files/code/HGAppDelegate.m
@@ -156,7 +156,6 @@
     // Store the deviceToken in the current installation and save it to Parse.
     PFInstallation *currentInstallation = [PFInstallation currentInstallation];
     [currentInstallation setDeviceTokenFromData:deviceToken];
-    currentInstallation.channels = @[@"global"];
     [currentInstallation saveInBackground];
 
 }
